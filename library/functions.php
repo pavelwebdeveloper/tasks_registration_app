89eabc65b28b371263e24a027b406ce818121e47
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-11-18 00:09:33 +0200

    ordering the code, removing repeating code, adding email verification

--- library/functions.php
+++ library/functions.php
@@ -2,40 +2,46 @@
 
 // a library of custom functions for tasks registration
 
-
-
 // the function to build the task table
-
 function buildTaskTable($tasks){
+ 
  // Build the tasks table using the $tasks array
- if(count($tasks) > 0){
-   
-  /*
-    $tasksTable = '<input type="hidden" id="sort" value="asc"><table class="table table-hover">';
-    $tasksTable .= '<thead>';
-    $tasksTable .= "<tr><th>ФИО исполнителя</th><th>название задачи</th><th id='sortTaskStartDate'>дата создания задачи</th><th  id='sortTaskFinishDate'>дата завершения задачи</th><th>описание задачи</th><th></th></tr>";
-    $tasksTable .= '</thead>';
-   * 
-   */
-    $tasksTable = "<table><tbody>";
-    foreach ($tasks as $task) {
-     $tasksTable .= "<tr><td>$task[doerName]</td>";
-     $tasksTable .= "<td>$task[taskName]</td>";
-     $tasksTable .= "<td>$task[taskStartDate]</td>";
-     $tasksTable .= "<td>$task[taskFinishDate]</td>";
-     $tasksTable .= "<td>$task[taskDescription]</td>";
-     $tasksTable .= "<td><a class='tablelink' onclick='deleteTask($task[taskId])'>Delete</a></td></tr>";
-    }
-    $tasksTable .= "</tbody></table>";
-    return $tasksTable;
-     
-   } else {
-    $tasksTable = '<p>Sorry, no tasks were found.</p>';
-    return $tasksTable;
-   }   
+ if(count($tasks) > 0){   
+  
+        $tasksTable = "<table><tbody>";
+        foreach ($tasks as $task) {
+         $tasksTable .= "<tr><td>$task[doerName]</td>";
+         $tasksTable .= "<td>$task[taskName]</td>";
+         $tasksTable .= "<td>$task[taskStartDate]</td>";
+         $tasksTable .= "<td>$task[taskFinishDate]</td>";
+         $tasksTable .= "<td>$task[taskDescription]</td>";
+         $tasksTable .= "<td><a class='tablelink' onclick='deleteTask($task[taskId])'>Delete</a></td></tr>";
+        }
+        $tasksTable .= "</tbody></table>";
+        return $tasksTable;
+
+       } else {
+        $tasksTable = '<p>Sorry, no tasks were found.</p>';
+        return $tasksTable;
+       }   
 }
 
-function sortTable($g){
+// the function to form the output for the frontend
+function formingOutput($message, $tasksTable){
  
+        $resultArray = array($message, $tasksTable);
+
+        $result = json_encode($resultArray);
+
+        echo $result;
+
+        exit;
+}
+
+function checkDoerName($doerName) {
+ $pattern = '/^[A-Za-z\x{0400}-\x{04FF}\s]{3,}$/';
+ $pattern = "/[A-Za-z\p{L}][^0-9]+$/u";
+ $pattern = "/[^0-9]+$/u";
+ return preg_match($pattern, $doerName);
 }
 
--- views/tasks-list.php
+++ views/tasks-list.php
@@ -8,259 +8,169 @@
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <style>
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-  width: 100%;
-  border: 1px solid #ddd;
-}
+        table {
+          border-collapse: collapse;
+          border-spacing: 0;
+          width: 100%;
+          border: 1px solid #ddd;
+        }
 
-th, td {
-  text-align: center;
-  width: 150px;
-  padding: 16px;
-}
+        th, td {
+          text-align: center;
+          width: 150px;
+          padding: 16px;
+        }
 
-tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
+        tr:nth-child(even) {
+          background-color: #f2f2f2;
+        }
 
-input#searchInput{
- width: 300px;
-}
-</style>
+        input#searchInput{
+         width: 300px;
+        }
+  </style>
   <script>
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
-  
-   
-    
-    
-   $(function() { 
-    $("#sortTaskFinishDate").click(function(){
-     var column = "taskFinishDate";
-     sortColumn(column);
-     });
-    });
-    
-    $(function() { 
-    $("#sortTaskStartDate").click(function(){
-     var column = "taskStartDate";
-     sortColumn(column);
-     });
-    });
-    
-    
-    function sortColumn(column){
-           var sortOrder = $("#sort").val();
-     
-     console.log(column);
-     console.log(sortOrder);
-   
-  /*
-  $.get("/phpprojects/tasks_registration_app/index.php?action=sortColumn&column="+column +"&sortOrder="+sortOrder, function(data, status){
-    if (status == 200) {
-        document.getElementById("tasks").innerHTML = this.responseText;
-      } else {
-       document.getElementById("tasks").innerHTML = "The request is being processed............";
-      }
-      if(sort == "asc"){
-     $("#sort").val("desc");
-   }else{
-     $("#sort").val("asc");
-   }
-  }); 
-  */
-  
-  
-  var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         try {
-           var obj = JSON.parse(xmlhttp.responseText);
-           
-           console.log(obj);
-           console.log(obj[0]);
-           console.log(obj[1]);
+         $(function() {
+           $( "#datepicker" ).datepicker();
+         }); 
 
-           document.getElementById("message").innerHTML = obj[0];
-           document.getElementById("tasks").innerHTML = obj[1];
 
-       } catch (error) {
-           throw Error;
-       }/* else {
-       document.getElementById("tasks").innerHTML = "The request is being processed............";
-      }*/
-       if(sortOrder == "asc"){
-     $("#sort").val("desc");
-   }else{
-     $("#sort").val("asc");
-   }
-  }
-    };
-    xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action=sortColumn&column="+column +"&sortOrder="+sortOrder, true);
-    xmlhttp.send();
-     }
+          $(function() { 
+                $("#sortTaskFinishDate").click(function(){
+                 var column = "taskFinishDate";
+                 sortColumn(column);
+              });
+           });
+
+           $(function() { 
+                $("#sortTaskStartDate").click(function(){
+                var column = "taskStartDate";
+                sortColumn(column);
+              });
+           });
+    
+    
+           function sortColumn(column){
+
+              var sortOrder = $("#sort").val();
+              var xmlhttp = new XMLHttpRequest();
+
+              xmlhttp.onreadystatechange = function() {
+             if (this.readyState == 4 && this.status == 200) {
+                try {
+
+                  var obj = JSON.parse(xmlhttp.responseText);
+                  document.getElementById("message").innerHTML = obj[0];
+                  document.getElementById("tasks").innerHTML = obj[1];
+
+              } catch (error) {
+                  throw Error;
+              }
+
+              if(sortOrder == "asc"){
+                 $("#sort").val("desc");
+             }else{
+                 $("#sort").val("asc");
+                  }
+                }
+              };
+                xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action=sortColumn&column="+column +"&sortOrder="+sortOrder, true);
+                xmlhttp.send();
+                 }
    
   </script>
  </head>
  <body>
   
   <main id="main">
-   <div class="container">
-  <div class="jumbotron">
+    <div class="container">
+    <div class="jumbotron">
       
-   <!--<form action="/phpprojects/tasks_registration_app/index.php" method="post">
-    <fieldset>-->
-   <p id="rp">All fields are required.</p>
-     <label for="doerName">ФИО исполнителя</label>
-     <input type="text" name="doerName" id="doerName" pattern="[A-Za-z]{2,}" <?php if(isset($doerName)){echo "value='$doerName'";} ?>  required>
-     <label for="eMail">E-mail</label>
-     <input type="eMail" name="eMail" id="eMail" placeholder="someone@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{2,}$" <?php if(isset($eMail)){echo "value='$eMail'";} ?> required>
-     <p><label for="datepicker">дата завершения задачи:</label> <input type="text" name="datepicker" id="datepicker"></p>
-     <label for="taskName">название задачи</label>
-     <input type="text" name="taskName" id="taskName" pattern="[A-Za-z]{2,}" <?php if(isset($taskName)){echo "value='$taskName'";} ?>  required>
-     <label>описание задачи</label>
-     <textarea name="taskDescription" id="taskDescription" rows="10" cols="43" placeholder="Please, describe the new product." required><?php if(isset($taskDescription)) {echo "$taskDescription";} ?></textarea>
-     <button class="submitBtn" name="submit" type="submit" value="Register" onclick="getTasks()">Register</button>
-     <p id="message"></p>
-     <!-- Add the action name - value pair 
-     <input type="hidden" name="action" value="Register">
-    </fieldset>
-    
-
-   </form>-->
    
+         <p id="rp">Все поля обязательны.</p>
+         <label for="doerName">ФИО исполнителя</label>
+         <input type="text" name="doerName" id="doerName" pattern="[A-Za-z]{2,}" <?php if(isset($doerName)){echo "value='$doerName'";} ?>  required>
+         <label for="eMail">E-mail</label>
+         <input type="eMail" name="eMail" id="eMail" placeholder="someone@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{2,}$" <?php if(isset($eMail)){echo "value='$eMail'";} ?> required>
+         <p><label for="datepicker">дата завершения задачи:</label> <input type="text" name="datepicker" id="datepicker"></p>
+         <label for="taskName">название задачи</label>
+         <input type="text" name="taskName" id="taskName" pattern="[A-Za-z]{2,}" <?php if(isset($taskName)){echo "value='$taskName'";} ?>  required>
+         <label>описание задачи</label>
+         <textarea name="taskDescription" id="taskDescription" rows="10" cols="43" placeholder="Пожалуйста, опишите новую задачу" required><?php if(isset($taskDescription)) {echo "$taskDescription";} ?></textarea>
+         <button class="submitBtn" name="submit" type="submit" value="Register" onclick="getTasks()">Register</button>
+         <p id="message"></p>
+        
+     </div>
+     </div>
    
-   </div>
-    </div>
-   
-  </main>
+   </main>
   
-  <div id="dataArea">
-  <input type="text" id="searchInput" onkeyup="searchTasks()" placeholder="Search for person or task names.." title="Type in a name">
-   <input type="hidden" id="sort" value="asc"><table>
-   <thead>
-   <tr><th>ФИО исполнителя</th><th>название задачи</th><th id='sortTaskStartDate'>дата создания задачи</th><th  id='sortTaskFinishDate'>дата завершения задачи</th><th>описание задачи</th><th></th></tr>
-   </thead></table><div id="tasks"><?php   echo $tasksTable;  ?></div>
-   </div>
+     <div id="dataArea">
+     <input type="text" id="searchInput" onkeyup="searchTasks()" placeholder="Search for person or task names.." title="Type in a name">
+      <input type="hidden" id="sort" value="asc"><table>
+      <thead>
+      <tr><th>ФИО исполнителя</th><th>название задачи</th><th id='sortTaskStartDate'>дата создания задачи</th><th  id='sortTaskFinishDate'>дата завершения задачи</th><th>описание задачи</th><th></th></tr>
+      </thead></table><div id="tasks"><?php   echo $tasksTable;  ?></div>
+      </div>
   
   <script>
    
 
   function getTasks() {
    
- var action = "register";
- var doerName = document.getElementById("doerName").value;
- var eMail = document.getElementById("eMail").value;
- var taskFinishDate = document.getElementById("datepicker").value;
- var taskName = document.getElementById("taskName").value;
- var taskDescription = document.getElementById("taskDescription").value;
-  console.log(doerName);
-    console.log(eMail);
-    console.log(taskFinishDate);
-    console.log(taskName);
-    console.log(taskDescription);
+       var action = "register";
+       var doerName = document.getElementById("doerName").value;
+       var eMail = document.getElementById("eMail").value;
+       var taskFinishDate = document.getElementById("datepicker").value;
+       var taskName = document.getElementById("taskName").value;
+       var taskDescription = document.getElementById("taskDescription").value;  
  
- 
-   var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       //console.log(this.responseText);
-       //document.getElementById("tasks").innerHTML = this.responseText;
-       
-       try {
-           var obj = JSON.parse(xmlhttp.responseText);
-           
-           console.log(obj);
-           console.log(obj[0]);
-           console.log(obj[1]);
-
-           document.getElementById("message").innerHTML = obj[0];
-           document.getElementById("tasks").innerHTML = obj[1];
-
-       } catch (error) {
-           throw Error;
-       }
-       
-              
-      } /*else {
-       document.getElementById("dataArea").innerHTML = "The request is being processed............";
-      }*/
-    };
-    xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&doerName="+doerName+"&eMail="+eMail+"&taskFinishDate="+taskFinishDate+"&taskName="+taskName+"&taskDescription="+taskDescription, true);
-    xmlhttp.send();
+       var xmlhttp = new XMLHttpRequest();
+        formOutput(xmlhttp);
+        
+        xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&doerName="+doerName+"&eMail="+eMail+"&taskFinishDate="+taskFinishDate+"&taskName="+taskName+"&taskDescription="+taskDescription, true);
+        xmlhttp.send();
    
     }
     
     function deleteTask(taskId) {
    
- var action = "delete";
+          var action = "delete";
  
  
-   var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         try {
-           var obj = JSON.parse(xmlhttp.responseText);
-           
-           console.log(obj);
-           console.log(obj[0]);
-           console.log(obj[1]);
-
-           document.getElementById("message").innerHTML = obj[0];
-           document.getElementById("tasks").innerHTML = obj[1];
-
-       } catch (error) {
-           throw Error;
-       } /*else {
-       document.getElementById("tasks").innerHTML = "The request is being processed............";
-      }*/
-      }
-    };
-    xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&taskId="+taskId, true);
-    xmlhttp.send();
-   
-    }
-   
+         var xmlhttp = new XMLHttpRequest();
+          formOutput(xmlhttp);
+         
+         xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&taskId="+taskId, true);
+         xmlhttp.send();
+         }   
    
    function searchTasks() {
    
- var action = "search";
- var searchValue = document.getElementById("searchInput").value;
+       var action = "search";
+       var searchValue = document.getElementById("searchInput").value;
  
-    console.log(searchValue);
- 
- 
-   var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        try {
-           var obj = JSON.parse(xmlhttp.responseText);
-           
-           console.log(obj);
-           console.log(obj[0]);
-           console.log(obj[1]);
+       var xmlhttp = new XMLHttpRequest();
+       formOutput(xmlhttp);
+       
+         xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&searchValue="+searchValue, true);
+         xmlhttp.send();
+         }  
+         
+         function formOutput(xmlhttp){
+          xmlhttp.onreadystatechange = function() {
+           if (this.readyState == 4 && this.status == 200) {
+              try {
+                var obj = JSON.parse(xmlhttp.responseText);
+               document.getElementById("message").innerHTML = obj[0];
+               document.getElementById("tasks").innerHTML = obj[1];
 
-           document.getElementById("message").innerHTML = obj[0];
-           document.getElementById("tasks").innerHTML = obj[1];
-
-       } catch (error) {
-           throw Error;
-       }/* else {
-       document.getElementById("tasks").innerHTML = "The request is being processed............";
-      }*/
-  }
-    };
-    xmlhttp.open("GET", "/phpprojects/tasks_registration_app/index.php?action="+action +"&searchValue="+searchValue, true);
-    xmlhttp.send();
-   
-    }
-    
-    
-  
-  
+            } catch (error) {
+                throw Error;
+            } 
+           }
+         };
+         }
 </script>
   
  </body>
